91fe1c060954cb1f2ecb09876cf2f9270c2495fd
Jdon <jdon@users.noreply.github.com>
2017-12-02 15:13:23 +0000

    Adding bilinear

--- Task1.m
+++ Task1.m
@@ -20,15 +20,19 @@ title('Step-2: Conversion of input image to greyscale');
 [orgHeight,orgWidth] = size(Igray);
 
 % set new height and width
-newHeight = 1836;
-newWidth = 1668;
+newHeight = 1668;
+newWidth = 1836;
 
 % work out how much to scale image
 HeightScaleFactor = newHeight/orgHeight;
 WidthScaleFactor = newWidth/orgWidth;
+disp(HeightScaleFactor);
+disp(WidthScaleFactor);
 
 % create new image, matrix filled with zeros
 NNImage(1:newHeight,1:newWidth)= 0;
+
+BiImage(1:newHeight,1:newWidth)= 0;
 
 % loop through height and width of new image
 for h = 1:newHeight
@@ -49,8 +53,35 @@ for h = 1:newHeight
         NNImage(h,w) = pixelValue;
     end
 end
+
+%loop though new image and put where zebra is
+hh = 1;
+ww = 1;
+for h = 1:newHeight-1
+    if(mod(h,HeightScaleFactor) == 0)
+        hh = hh+1;
+    end
+    for w = 1:newWidth-1
+        if(mod(h,HeightScaleFactor) == 0 && mod(w,WidthScaleFactor) == 0)
+            BiImage(h,w) = Igray(hh,ww);
+            ww = ww+1;
+            if(ww >= orgWidth) %rest the pixel width to 1
+                ww = 1;
+            end
+        end
+    end
+end
+for h = 1:newHeight
+    for w = 1:newWidth
+        
+    end
+end
 % conver to image and display it
 nn = mat2gray(NNImage);
 figure;
 imshow(nn);
 title('Step-3: NN interpolation');
+bi = mat2gray(BiImage);
+figure;
+imshow(bi);
+title('Step-3: BiImage interpolation');
