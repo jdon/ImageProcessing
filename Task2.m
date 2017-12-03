b94a3b4af55338c4909c8611d7c520e4fcb97b0f
Jdon <jdon@users.noreply.github.com>
2017-12-03 17:04:41 +0000

    Added piecewise-linear transformation

--- Task2.m
+++ Task2.m
@@ -1,5 +1,31 @@
 % MATLAB script for Assessment Item-1
 % Task-2
 clear; close all; clc;
+% Step-1: Load input image
+Igray = imread('SC.png');
+% Step-2: Conversion of input image to grey-scale image
+%Igray = rgb2gray(I);
+[orgHeight,orgWidth] = size(Igray);
+
+NewImage(1:orgHeight,1:orgWidth)= 0;
+for h = 1:orgHeight
+    for w = 1:orgWidth
+        pixelvalue = Igray(h,w);
+        if (pixelvalue >= 80 && 100 <= pixelvalue)
+            NewImage(h,w) = 220;
+        else
+            NewImage(h,w) = pixelvalue;
+        end
+    end
+end
+% conver to image and display it
+figure;
+imshow(Igray);
+title('Old Image');
+nI = mat2gray(NewImage);
+figure;
+imshow(nI);
+title('New Image');
+
 
 
