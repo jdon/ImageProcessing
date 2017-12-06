6b2432c283d922cdf72bbb87562df022d0ce51c7
Jdon <jdon@users.noreply.github.com>
2017-12-06 13:17:54 +0000

    Added comments

--- Task1.m
+++ Task1.m
@@ -2,17 +2,8 @@
 % Task-1
 clear; close all; clc;
 
-% Step-1: Load input image
 I = imread('Zebra.jpg');
-figure;
-imshow(I);
-title('Step-1: Load input image');
-
-% Step-2: Conversion of input image to grey-scale image
 Igray = rgb2gray(I);
-figure;
-imshow(Igray);
-title('Step-2: Conversion of input image to greyscale');
 
 % and following steps
 
@@ -35,86 +26,74 @@ Igraypadded = padarray(Igray,[1 1],'symmetric');
 NNImage(1:newHeight,1:newWidth)= 0;
 
 BiImage(1:newHeight,1:newWidth)= 0;
-upscaledValues(1:newHeight,1:newWidth)= 0;
+BiLinear(1:newHeight,1:newWidth)= 0;
 % loop through height and width of new image
+tic;
 for h = 1:newHeight
     for w = 1:newWidth
         % get what pixel x,y to use on new image
-        pixelLocationH = round(h/HeightScaleFactor);
-        pixelLocationW = round(w/WidthScaleFactor);
-        % if rounded to zero, change to 1, as matlab is based on 1 index
-        if pixelLocationH == 0
-            pixelLocationH = 1;
-        end
-        if pixelLocationW == 0
-            pixelLocationW = 1;
-        end
-        % get value from image
+        pixelLocationH = ceil(h/HeightScaleFactor);
+        pixelLocationW = ceil(w/WidthScaleFactor);
+        % get value from original image
         pixelValue = Igray(pixelLocationH,pixelLocationW);
         % set value to new image
         NNImage(h,w) = pixelValue;
     end
 end
+disp(toc);
+tic;
 for h = 1:orgHeight
     for w = 1:orgWidth
-        upscaledValues((3*h)-2,(3*w)-2) = Igray(h,w);
+        % set pixels from old image into new image
+        BiLinear((3*h)-2,(3*w)-2) = Igray(h,w);
     end
 end
-upscaledValuesPadded = padarray(upscaledValues,[1 1],255);
-upscaledValues = upscaledValuesPadded(2:newHeight+2,2:newWidth+2);
-%loop though new image and put where zebra is
+%pad the new bilinear image 
+BiLinearPadded = padarray(BiLinear,[1 1],255);
+%change the padded image, so that right and bottom of image is padded
+BiLinear = BiLinearPadded(2:newHeight+2,2:newWidth+2);
+%loop though the bilinear image and perform linear interpolation on the top
+%and bottom of each unknown 9*9
 for h = 1:newHeight
     for w = 1:newWidth
+        % get the four points where the pixels from the small image are in
+        % the new larger image
         w1 = 3*(floor((w-1)/3))+1;
         w2 = 3*(ceil((w-1)/3))+1;
         h1 = 3*(ceil((h-1)/3))+1;
         h2 = 3*(floor((h-1)/3))+1;
+        %creat cords for the four corners
         currentcords = [h,w];
+        
         bottomleftcords=[h1,w1];
         topleftcords=[h2,w1];
         bottomrightcords=[h1,w2];
         toprightcords=[h2,w2];
-        
-        bottomleft=upscaledValues(h1,w1);
-        topleft=upscaledValues(h2,w1);
-        bottomright=upscaledValues(h1,w2);
-        topright=upscaledValues(h2,w2);
-        
+        %get the pixel values for the top corners
+        topleft=BiLinear(h2,w1);
+        topright=BiLinear(h2,w2);
+        % get the distance from the current pixel to pixels in the original
+        % 
         distLeft = w -topleftcords(2);
         distRight = toprightcords(2) -w;
-        test = w2-w;
-        test1 = w2-w1;
-        tes2 = test/test1;
-        topleftscale = topleft*tes2;
-        bottomleftscale = bottomleft * tes2;
+        % work out what weighting to use for each pixel from the original
+        % image
+        leftScale = (w2-w)/(w2-w1);
+        rightScale = (w -w1)/(w2-w1);       
+        %Work out weighting of the left and right corners in regards to the
+        %postion of the current pixel
+        leftValue = topleft*leftScale;
+        rightValue = topright*rightScale;
         
-        test3 = w -w1;
-        test4 = w2-w1;
-        test5 = test3/test4;
-        toprightscale = topright*test5;
-        botrightscale = bottomright * test5;
-        
-        topValue = topleftscale +toprightscale;
-        bottomValue = bottomleftscale +botrightscale;
-        
-        %leftValue = ((h2 - h)/(h2 - h1))*topleft + ((h - h1)/(h2 - h1))*topright;
-        %bilinear = ((h2 - h)/(h2 - h1))*topValue + ((h - h1)/(h2 - h1))*bottomValue;
-        %BiImage(h,w) = bilinear;
-        %upscaledValues(h,w) = bilinear;
+        value = leftValue +rightValue;
+        % only set the values in the new image, if they are not going to
+        % overlap the original values
         if(isequal(bottomleftcords,currentcords)||  isequal(bottomrightcords,currentcords)||  isequal(toprightcords,currentcords)||  isequal(topleftcords,currentcords))
         else
-            %upscaledValues(h,w) = bilinear;
             if(isequal(bottomleftcords,topleftcords)&&  isequal(bottomrightcords,toprightcords))
-                %do linear on top values
-                upscaledValues(h,w) = topValue;
-            end
-            if(isequal(bottomleftcords,bottomrightcords)&&  isequal(topleftcords,toprightcords))
-                %do linear on left values
-                %upscaledValues(h,w) = leftValue;
-            end
-            if (isnan(topValue))
-            else
-                %BiImage(h,w) = topValue;
+                % only set the values in the new image, if they are not going to
+                % overlap the original values
+                BiLinear(h,w) = value;
             end
         end
         
@@ -129,30 +108,25 @@ for h = 1:newHeight
         
         topCord = [topH,w];
         bottomCord = [bottomH,w];
-        topValue = upscaledValues(topH,w);
-        bottomValue = upscaledValues(bottomH,w);
+        topValue = BiLinear(topH,w);
+        bottomValue = BiLinear(bottomH,w);
+        
         if(h ~= topH || h ~= bottomH)
             topish = (topH - h)/(topH-bottomH)*bottomValue;
             bottomish = (h-bottomH)/(topH-bottomH)*topValue;
-            upscaledValues(h,w) = topish + bottomish;
+            BiLinear(h,w) = topish + bottomish;
         end
         
     end
 end
-upscaledValues = upscaledValues(1:newHeight,1:newWidth);
-for h = 1:newHeight
-    for w = 1:newWidth
-        
-    end
-end
+BiLinear = BiLinear(1:newHeight,1:newWidth);
+disp(toc);
 %gg = GetBilinearPixel(Igray,250,500);
 % conver to image and display it
 nn = mat2gray(NNImage);
-figure;
-imshow(nn);
-title('Nearest Neighbour');
-ups = mat2gray(upscaledValues);
-figure;
-imshow(ups);
-title('BiImage interpolation');
+ups = mat2gray(BiLinear);
+subplot(1,2,1), imshow(nn)
+title('Nearest Neighbour')
+subplot(1,2,2), imshow(ups)
+title('Bilinear')
 
